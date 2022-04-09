3a6bf981b1e8b0fc0e24b4de3a0a9b1545fce61a
kkrogs <kourtney.krogman@gmail.com>
2022-04-09 10:32:08 -0600

    pushl request for backend

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,6 @@ const userRoutes = require('./userRoutes');
 const projectRoutes = require('./petRoutes');
 //http://localhost:3001/api/ is our current route. If I see /api that means I am doing some kind of data exchange. In this case, we're going to the database
 router.use('/users', userRoutes);
-router.use('/pets', projectRoutes);
+router.use('/pets', petsRoutes);
 
 module.exports = router;
