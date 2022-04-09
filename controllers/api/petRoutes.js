ba2bf5365122aeb86c81354f039f0b0c023f37a7
kkrogs <kourtney.krogman@gmail.com>
2022-04-09 16:45:52 -0600

    changes

--- controllers/api/petRoutes.js
+++ controllers/api/petRoutes.js
@@ -2,37 +2,109 @@ const router = require('express').Router();
 const { Pet } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
-  try {
-    const newPet = await Pet.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
+//new code
+// once they signup from the signup form for pets, the form data is sent to this api users POST -- see first post below
+//The current route for this file is: http://localhost:3001/api/pets
 
-    res.status(200).json(newPet);
+router.post('/', async (req, res) => {
+  console.log("petsRoutes")
+  try {
+    const petData = await Pet.create(req.body);
+    console.log("petData")
+    console.log(petData)
+    req.session.save(() => {
+      req.session.pet_id = petData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(petData);
+    });
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
-  try {
-    const petData = await Pet.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
 
-    if (!petData) {
-      res.status(404).json({ message: 'No pet found with this id!' });
-      return;
-    }
 
-    res.status(200).json(petData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+
+
+
+
+
+
+// router.get('/pet', withAuth, async(req,res) => {
+
+//   try {
+//     const petData = await Pet.findAll(
+//       {
+//         include: [{ model: Person }
+//       }
+//     );
+
+//     if (!petData) {
+//       res.status(404).json({ message: 'There was no information' });
+//       return;
+//     }
+
+//     res.status(200).json(petData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// old code
+// router.post('/', withAuth, async (req, res) => {
+//   try {
+//     const newPet = await Pet.create({
+//       ...req.body,
+//       user_id: req.session.user_id,
+//     });
+
+//     res.status(200).json(newPet);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+
+// router.delete('/pet', withAuth, async (req, res) => {
+//   try {
+//     const petData = await Pet.destroy({
+//       where: {
+//         id: req.params.id,
+//         user_id: req.session.user_id,
+//       },
+//     });
+
+//     if (!petData) {
+//       res.status(404).json({ message: 'No pet found' });
+//       return;
+//     }
+
+//     res.status(200).json(petData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;
